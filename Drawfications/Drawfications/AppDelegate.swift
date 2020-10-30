938c60a40b9ae142f1e41c93f58d0fe9f18dc7fb
UInt2048 <MBenedict2004@gmail.com>
2020-10-30 17:39:33 -0500

    Initial corrections

--- Drawfications/Drawfications/AppDelegate.swift
+++ Drawfications/Drawfications/AppDelegate.swift
@@ -8,11 +8,7 @@
 import UIKit
 import CoreData
 
-@main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+@main class AppDelegate: UIResponder, UIApplicationDelegate {
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
     return true
@@ -76,6 +72,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
           }
       }
   }
-
 }
 
